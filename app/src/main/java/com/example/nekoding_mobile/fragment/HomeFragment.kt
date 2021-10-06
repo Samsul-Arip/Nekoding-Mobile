67619f1523031bcb37862550ed0ba3e0a435efda
Samsul-Arp <bajevky@gmail.com>
2021-10-06 11:38:10 +0700

    new design

--- app/src/main/java/com/example/nekoding_mobile/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/nekoding_mobile/fragment/HomeFragment.kt
@@ -1,8 +1,10 @@
 package com.example.nekoding_mobile.fragment
 
 import android.app.ActionBar
+import android.app.Activity
 import android.os.Bundle
 import android.os.Handler
+import android.transition.Slide
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,11 +14,14 @@ import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
+import com.example.nekoding_mobile.MainActivity
 import com.example.nekoding_mobile.R
 import com.example.nekoding_mobile.SlideAdapter
 import com.example.nekoding_mobile.SliderItems
 import com.example.nekoding_mobile.databinding.FragmentHomeBinding
 import java.lang.Math.abs
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class HomeFragment : Fragment() {
@@ -26,8 +31,10 @@ class HomeFragment : Fragment() {
 
     private lateinit var myDataList : ArrayList<SliderItems>
     private lateinit var slideAdapter: SlideAdapter
-    lateinit var viewPager : ViewPager
 
+    var i: Int = 0;
+    private val runnable: Runnable? = null
+    private val handler: Handler = Handler()
 
 
     override fun onCreateView(
@@ -68,18 +75,38 @@ class HomeFragment : Fragment() {
         myDataList = ArrayList()
         myDataList.add(SliderItems(R.drawable.slide_satu))
         myDataList.add(SliderItems(R.drawable.slide_dua))
+        myDataList.add(SliderItems(R.drawable.slide_satu))
+        myDataList.add(SliderItems(R.drawable.slide_dua))
 
         slideAdapter = SlideAdapter(requireContext(), myDataList)
 
         binding.viewPager.adapter = slideAdapter
-
         binding.apply {
-            viewPager.setPadding(25,0,200,0)
+            viewPager.setPadding(25,0,200,10)
             viewPager.clipToPadding = false
             viewPager.pageMargin = 25
-
         }
 
+        startAutoSlide(slideAdapter.count)
+
     }
+
+
+    private fun startAutoSlide(count: Int) {
+        val runnable = Runnable {
+            if(slideAdapter.count == count) {
+                i = 0
+            } else {
+                i++
+            }
+            binding.viewPager.setCurrentItem(i, true)
+            if (runnable != null) {
+                handler.postDelayed(runnable, 3000)
+            }
+        }
+        handler.postDelayed(runnable, 3000)
+    }
+
+
 
 }
